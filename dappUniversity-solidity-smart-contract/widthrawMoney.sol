706ac46f4294f8b9974f1ea676827ce9b1d5886b
tiff0000 <tiffanyy.dev@gmail.com>
2022-09-01 01:05:29 -0400

    add a withdraw function with send_to address

--- dappUniversity-solidity-smart-contract/widthrawMoney.sol
+++ dappUniversity-solidity-smart-contract/widthrawMoney.sol
@@ -1,20 +1,23 @@
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
 
-contract WithdrawMoney {
-    mapping(address => uint) public balanceReceived;
+contract SendMoney {
+    uint public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived += msg.value;
+    }
 
     function getBalance() public view returns(uint) {
         return address(this).balance;
     }
 
-    function sendMoney() public payable {
-        balanceReceived[msg.sender] += msg.value;
+    function withdrawMoney() public {
+        address payable to = payable(msg.sender);
+        to.transfer(this.getBalance());
     }
 
-    function withdrawAllMoney(address payable _to) public {
-        uint balanceToSend = balanceReceived[msg.sender];
-        balanceReceived[msg.sender] = 0;
-        _to.transfer(balanceToSend);
-        // _to.transfer(address(this).balance);
+    function withDrawMoneyTo(address payable _to)  public {
+        _to.transfer(this.getBalance());
     }
 }
